0b1843ee0d2d21cdb32dbc4f78dd251ae4862e39
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-09-09 18:18:01 +0200

    logic home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
+import { useState } from "react";
 import { Col, Container, Image, Nav, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    if (username && password) {
+      navigate("/recipes");
+    } else {
+      alert("Please enter both username and password.");
+    }
+  };
+
   return (
     <Container>
       <Image
-        src="https://static.wixstatic.com/media/2524ce_cb266239a431485aa92c419e50505f7a~mv2.png"
+        src="https://img.freepik.com/free-photo/close-up-volunteers-collecting-food_23-2149182026.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717545600&semt=ais_user"
         className="mx-auto d-block"
       />
       <Row>
-        <h1 className="text-center">HYF Frontend template</h1>
+        <h1 className="text-center">CareFood</h1>
         <Col>
-          <Nav.Link href="/recipes" className="text-center">
-            Recipes List
-          </Nav.Link>
+          <div className="text-center">
+            <label>
+              username:
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+              ></input>
+            </label>
+            <label>
+              password:
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="text"
+              ></input>
+            </label>
+            <button onClick={handleLogin}>
+              Login
+            </button>
+          </div>
         </Col>
       </Row>
     </Container>
