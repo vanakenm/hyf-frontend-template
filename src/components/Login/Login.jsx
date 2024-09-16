c762b01cae88b2f3e8119bd1165625e076b28e30
Glorius King <gloriusking3@gmail.com>
2024-09-17 00:02:26 +0200

    modified login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,8 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
-<<<<<<< HEAD
 import { Nav } from "react-bootstrap";
 import "./login.css";
-=======
-import { Nav } from 'react-bootstrap';
-import './Login.css';
-
-const Login = ()=>{
->>>>>>> main
 
 const Login = () => {
   const {
@@ -21,58 +14,48 @@ const Login = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-<<<<<<< HEAD
-    <div className="container">
-      <Form onSubmit={handleSubmit(onSubmit)} className="p-4 login-style">
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            {...register("email", { required: true })}
-            type="email"
-            placeholder="Enter email"
-          />
-=======
-  <Form onSubmit={handleSubmit(onSubmit)} className="form-container">
+    <Form onSubmit={handleSubmit(onSubmit)} className="form-container">
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control {...register("email", { required: true })}type="email" placeholder="Enter email" />
-        
+        <Form.Control
+          {...register("email", { required: true })}
+          type="email"
+          placeholder="Enter email"
+        />
+
         {errors.email && errors.email.type === "required" && (
-        <span className='text-danger'> Email is required</span>
-      )}
+          <span className="text-danger"> Email is required</span>
+        )}
       </Form.Group>
->>>>>>> main
 
-          {errors.email && errors.email.type === "required" && (
-            <span className="text-danger"> Email is required</span>
-          )}
-        </Form.Group>
+      {errors.email && errors.email.type === "required" && (
+        <span className="text-danger"> Email is required</span>
+      )}
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            {...register("password", { required: true })}
-            type="password"
-            placeholder="Password"
-          />
-          {errors.password && errors.password.type === "required" && (
-            <span className="text-danger"> Password is required</span>
-          )}
-        </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          {...register("password", { required: true })}
+          type="password"
+          placeholder="Password"
+        />
+        {errors.password && errors.password.type === "required" && (
+          <span className="text-danger"> Password is required</span>
+        )}
+      </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
 
-        <p>
-          <br></br>
-          if you do not have an account{" "}
-          <Nav.Link href="/register" className="text-center">
-            Register
-          </Nav.Link>
-        </p>
-      </Form>
-    </div>
+      <p>
+        <br></br>
+        if you do not have an account{" "}
+        <Nav.Link href="/register" className="text-center">
+          Register
+        </Nav.Link>
+      </p>
+    </Form>
   );
 };
 export default Login;
