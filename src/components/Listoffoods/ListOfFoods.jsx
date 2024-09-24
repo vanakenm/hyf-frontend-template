ded8a938c501945aa0118e4982527251e55a173e
Mykhailo Shevchenko <shevchenko21021984@gmail.com>
2024-09-24 20:56:56 +0200

    fix home page

--- src/components/Listoffoods/ListOfFoods.jsx
+++ src/components/Listoffoods/ListOfFoods.jsx
@@ -4,28 +4,32 @@ const ListOfFoods = ({ offers }) => {
   return (
     <section className="offer-container">
       {offers.map((item, index) => (
-        <article key={index} className="offer-item">
+        <article key={index} className="offer-items">
           <div className="offer-details">
             <ul>
               <li className="offer-info">
-                <span className="offer-label">Pickup Time: </span>
-                <span className="offer-value">{item.pickup_time}</span>
-              </li>
-              <li className="offer-info">
-                <span className="offer-label">Diabetic Quantity: </span>
-                <span className="offer-value">{item.diabetic_quantity}</span>
-              </li>
-              <li className="offer-info">
-                <span className="offer-label">Provider ID: </span>
-                <span className="offer-value">{item.provider_id}</span>
+                <span className="offer-label">Provider: </span>
+                <span className="offer-value">{item.provider_name}</span>
               </li>
               <li className="offer-info">
                 <span className="offer-label">Standard Quantity: </span>
-                <span className="offer-value">{item.standard_quantity}</span>
+                <span className="offer-value">{item.standard_unit}</span>
               </li>
               <li className="offer-info">
                 <span className="offer-label">Vegan Quantity: </span>
-                <span className="offer-value">{item.vegan_quantity}</span>
+                <span className="offer-value">{item.vegan_unit}</span>
+              </li>
+              <li className="offer-info">
+                <span className="offer-label">Diabetic Quantity: </span>
+                <span className="offer-value">{item.diabetic_unit}</span>
+              </li>
+              <li className="offer-info">
+                <span className="offer-label">Address: </span>
+                <span className="offer-value">{item.address}</span>
+              </li>
+              <li className="offer-info">
+                <span className="offer-label">Pickup Time: </span>
+                <span className="offer-value">{item.pickup_time}</span>
               </li>
             </ul>
           </div>
